c0899985a2181c4b0fe5a3d1213933eb8c89a85c
Pedro Torres <kpedro101@gmail.com>
2021-11-21 08:09:27 -0300

    funções

---
+++ src/Client.py
@@ -0,0 +1,38 @@
+import io
+import os
+
+from google.cloud import vision
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="res\Key.json"
+
+client = vision.ImageAnnotatorClient()
+
+def detect_labels(path):
+
+    file_name = os.path.abspath(path)
+
+
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    return labels
+
+def detect_text(path):
+
+    file_name = os.path.abspath(path)
+
+
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    return texts
